25a288e719a2bbff6337da9ddc3136ad48607230
paulohenrique001 <paulo001.life@gmai.com>
2022-05-04 23:31:03 -0300

    Teste commit

--- src/index.php
+++ src/index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP LiveReload - Padev</title>
 </head>
 
 <body>
-    <h1>Live reload PHP</h1>
+    <h1>PHP Live Reload - Padev</h1>
 
     <script type="module">
         import {
